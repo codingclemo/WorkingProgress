f475ce0bd7d278d0eaefc569a69449819c831365
codingclemo <clemens.kozmich@gmail.com>
2018-12-15 14:19:27 +0100

    adding files

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Hardware.Camera2;
+
+namespace TestZXing.Droid
+{
+    [Activity(Label = "TestZXing.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+
+            /*
+            var camera = global::Android.Hardware.Camera.Open(1);
+            var options = camera.GetParameters();
+        
+
+            if (options.IsZoomSupported)
+            {
+                options.Zoom = (options.MaxZoom / 2);
+                Console.WriteLine("Set zoom to " + options.Zoom);
+                camera.SetParameters(options);                             
+            }
+
+            camera.Release();
+            */
+            
+            /*
+            var manager = (CameraManager)GetSystemService(Context.CameraService);
+
+            for (var i = 0; i < manager.GetCameraIdList().Length; i++)
+            {
+                var cameraId = manager.GetCameraIdList()[i];
+                CameraCharacteristics characteristics = manager.GetCameraCharacteristics(cameraId);
+
+                Console.WriteLine(characteristics.Get(CameraCharacteristics.LensFacing).ToString());
+            }
+            */
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ TestZXing/TestZXing.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace TestZXing
+{
+    public class App : Application
+    {
+        ZXingScannerPage scanPage;
+
+        public App()
+        {
+            scanPage = new ZXingScannerPage();
+
+            scanPage.OnScanResult += (result) =>
+            {
+                //System.Diagnostics.Debug.WriteLine("Scanned datamatrix code -> [" + result.Text + "]");
+                
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Show the result
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    scanPage.DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+            
+            //MainPage needs to be set, otherwise error
+            MainPage = scanPage;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
